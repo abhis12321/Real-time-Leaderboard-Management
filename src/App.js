3dfcc9b2bcbec4f457ae64735d8dabd45d689804
abhis12321 <abhisheksingh2359328@gmail.com>
2024-02-02 12:26:09 +0530

    primary commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap-grid.min.css';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <Nav />
+      <div className='bg-warning text-primary border border-success mx-2 my-2 p-2 rounded d-flex justify-content-center align-items-center'>
+        <div className='para'>Hello World...!</div>
+        <button className='btn-success btn mx-2'>Booti</button>
+      </div>
+    </React.Fragment>
+  )
 }
 
-export default App;
+export default App
